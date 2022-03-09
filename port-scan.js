e7318bcaaaa65ea20a6a2341ed1b5c19598697ce
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-03-09 20:36:17 +0100

    add default value to port-scan arg

--- port-scan.js
+++ port-scan.js
@@ -1,7 +1,11 @@
 const { getHostIp, getOpenPorts, error } = require('./utils');
 
 (async function () {
-  const service = process.argv[2];
+  let service = process.argv[2];
+  if (!service) {
+    console.log("No network service specified as argument 1, defaulting to tcp");
+    service = "tcp"; 
+  }
   const {
     ip,
     mask
@@ -11,6 +15,7 @@ const { getHostIp, getOpenPorts, error } = require('./utils');
     .filter(e => e.ports.some(port =>
       port.status === 'open' && (service ? port.service === 'ftp' : true)
     ))
+  console.log(`Found ${hostsWithOpenPorts.length} hosts with open ${service} ports`);
   hostsWithOpenPorts.forEach(host => {
     const openPorts = host.ports.map(p => `${p.port}: ${p.service}/${p.protocol}`).join(', ');
     process.stdout.write(`${host.ip} has ${host.ports.length} open ports (${openPorts})\n`)
